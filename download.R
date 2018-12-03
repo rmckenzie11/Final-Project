4207d2c8116cd23c42e83ffe69b5ad1b1465ba8e
Robert <robertmckenzie@college.harvard.edu>
2018-12-03 08:55:04 -0500

    leaflet

--- download.R
+++ download.R
@@ -27,22 +27,22 @@ d <- file.path("C:\\Users\\Robert\\Desktop\\Gov 1005\\test\\data", c("yellow_tri
                                                                "yellow_tripdata_2017-12.csv"))
 
 
-f2 <- file.path("https://s3.amazonaws.com/nyc-tlc/trip+data", c("fhv_tripdata_2018-01.csv",
-                                                               "fhv_tripdata_2018-02.csv", 
-                                                               "fhv_tripdata_2018-03.csv", 
-                                                               "fhv_tripdata_2018-04.csv",
-                                                               "fhv_tripdata_2018-05.csv",
-                                                               "fhv_tripdata_2018-06.csv"))
+
+f2 <- file.path("https://canvas.harvard.edu/courses/45146/files", c(""))
 
 
-d2 <- file.path("C:\\Users\\Robert\\Desktop\\Gov 1005\\test\\data", c("fhv_tripdata_2018-01.csv","fhv_tripdata_2018-02.csv","fhv_tripdata_2018-03.csv","fhv_tripdata_2018-04.csv","fhv_tripdata_2018-05.csv","fhv_tripdata_2018-06.csv"))
+cab_data18 %>%
+  filter(! V9 == c("138", "132", "1")) %>%
+  filter(! V8 == c("138", "132", "1")) %>%
+  summarise(mean(V5))
+
+
+download.file()
+
+d <- file.path()
 
 for(i in 1:length(f)){
   download.file(f[i], d[i], mode = "wb")
-}
-
-for(i in 1:length(f2)){
-  download.file(f2[i], d2[i], mode = "wb")
 }
 
 library(data.table)
